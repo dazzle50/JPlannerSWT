bc7e86f30189be70946814b9633a5f4b44cfc803
Richard Crook <dazzle50@gmail.com>
2014-09-27 00:01:33 +0100

    Initial "Hello World!" commit

---
+++ src/rjc/jplanner/jplanner.java
@@ -0,0 +1,38 @@
+/**************************************************************************
+ *  Copyright (C) 2014 by Richard Crook                                   *
+ *  http://code.google.com/p/jplanner/                                    *
+ *                                                                        *
+ *  This program is free software: you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  This program is distributed in the hope that it will be useful,       *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with this program.  If not, see http://www.gnu.org/licenses/    *
+ **************************************************************************/
+
+package rjc.jplanner;
+
+/*************************************************************************************************/
+// JPlanner by Richard Crook
+// Aims to be a project planner similar to M$Project with table entry of tasks & Gantt chart
+// Also aims to have automatic resource levelling and scheduling based on task priority
+// Also aims to have resource levels variable within single task
+// Also aims to have Gantt chart task bar thickness showing this variable resource usage
+// Based on work I started as early as 2005
+/*************************************************************************************************/
+
+public class jplanner
+{
+
+  public static void main( String[] args )
+  {
+    System.out.println( "Hello World!" );
+  }
+
+}
