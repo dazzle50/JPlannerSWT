1b31646b2bcaf22b17e361f8b5b15e4a261d630d
Richard Crook <dazzle50@gmail.com>
2015-02-14 16:03:13 +0000

    Use Nebula NatTable instead of plain SWT Table

--- src/rjc/jplanner/gui/MainTabWidget.java
+++ src/rjc/jplanner/gui/MainTabWidget.java
@@ -1,22 +1,22 @@
 package rjc.jplanner.gui;
 
+import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
-import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.Text;
 
 public class MainTabWidget extends TabFolder
 {
-  private Text  text;
-  private Text  text_1;
-  private Table table;
-  private Table table_1;
-  private Table table_2;
-  private Table table_3;
+  private Text     tempPlanProperties;
+  private Text     tempPlanNotes;
+  private NatTable tableDays;
+  private NatTable tableCalendars;
+  private NatTable tableResources;
+  private NatTable tableTasks;
 
   /**
    * Create the composite.
@@ -30,53 +30,45 @@ public class MainTabWidget extends TabFolder
     TabItem tabPlan = new TabItem( this, SWT.NONE );
     tabPlan.setText( "Plan" );
 
-    SashForm sashForm = new SashForm( this, SWT.SMOOTH );
-    tabPlan.setControl( sashForm );
+    SashForm splitterPlanTab = new SashForm( this, SWT.SMOOTH );
+    tabPlan.setControl( splitterPlanTab );
 
-    text = new Text( sashForm, SWT.BORDER );
+    tempPlanProperties = new Text( splitterPlanTab, SWT.BORDER );
 
-    text_1 = new Text( sashForm, SWT.BORDER );
-    sashForm.setWeights( new int[] { 1, 1 } );
+    tempPlanNotes = new Text( splitterPlanTab, SWT.BORDER );
+    splitterPlanTab.setWeights( new int[] { 1, 1 } );
 
     TabItem tabTasksGantt = new TabItem( this, SWT.NONE );
     tabTasksGantt.setText( "Tasks && Gantt" );
 
-    SashForm sashForm_1 = new SashForm( this, SWT.SMOOTH );
-    tabTasksGantt.setControl( sashForm_1 );
+    SashForm splitterTasksGantt = new SashForm( this, SWT.SMOOTH );
+    tabTasksGantt.setControl( splitterTasksGantt );
 
-    table_3 = new Table( sashForm_1, SWT.BORDER | SWT.FULL_SELECTION );
-    table_3.setHeaderVisible( true );
-    table_3.setLinesVisible( true );
+    tableTasks = new NatTable( splitterTasksGantt );
 
-    ScrolledComposite scrolledComposite = new ScrolledComposite( sashForm_1, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL );
+    ScrolledComposite scrolledComposite = new ScrolledComposite( splitterTasksGantt, SWT.BORDER | SWT.H_SCROLL
+        | SWT.V_SCROLL );
     scrolledComposite.setExpandHorizontal( true );
     scrolledComposite.setExpandVertical( true );
-    sashForm_1.setWeights( new int[] { 1, 1 } );
+    splitterTasksGantt.setWeights( new int[] { 1, 1 } );
 
-    TabItem tbtmResources = new TabItem( this, SWT.NONE );
-    tbtmResources.setText( "Resources" );
+    TabItem tabResources = new TabItem( this, SWT.NONE );
+    tabResources.setText( "Resources" );
 
-    table_2 = new Table( this, SWT.BORDER | SWT.FULL_SELECTION );
-    tbtmResources.setControl( table_2 );
-    table_2.setHeaderVisible( true );
-    table_2.setLinesVisible( true );
+    tableResources = new NatTable( this );
+    tabResources.setControl( tableResources );
 
-    TabItem tbtmCalendars = new TabItem( this, SWT.NONE );
-    tbtmCalendars.setText( "Calendars" );
+    TabItem tabCalendars = new TabItem( this, SWT.NONE );
+    tabCalendars.setText( "Calendars" );
 
-    table_1 = new Table( this, SWT.BORDER | SWT.FULL_SELECTION );
-    tbtmCalendars.setControl( table_1 );
-    table_1.setHeaderVisible( true );
-    table_1.setLinesVisible( true );
+    tableCalendars = new NatTable( this );
+    tabCalendars.setControl( tableCalendars );
 
-    TabItem tbtmDays = new TabItem( this, SWT.NONE );
-    tbtmDays.setText( "Days" );
+    TabItem tabDays = new TabItem( this, SWT.NONE );
+    tabDays.setText( "Days" );
 
-    table = new Table( this, SWT.BORDER | SWT.FULL_SELECTION );
-    tbtmDays.setControl( table );
-    table.setHeaderVisible( true );
-    table.setLinesVisible( true );
-
+    tableDays = new NatTable( this );
+    tabDays.setControl( tableDays );
   }
 
   @Override
